610326d6e90bf73a55c44fd2652bf801ae13b490
Yuichi Yogo <yuichkun@gmail.com>
2021-11-18 21:34:01 +0900

    add test files

---
+++ src/__tests__/example.ts
@@ -0,0 +1,29 @@
+function returnsExplicitAny() {
+    // This should stay intact because explicity defined as any
+    return 3 as any
+}
+
+// This should stay intact because explicity defined as any
+const anyObject = {} as any
+
+export class TestClass {
+    referencedAny() {
+        return anyObject
+    }
+    // this should be annotated as unknown because it is not referenced
+    unreferencedAny() {
+        return anyObject
+    }
+
+    shouldFail() {
+        // this should fail because only any type can allow this
+        console.log(this.referencedAny().foo)
+    }
+
+    referencedImplicitAnyParam(params) {
+        // this should fail because only any type can allow this
+        console.log(params.foo)
+    }
+    // this should be annotated as unknown because it is not referenced
+    unreferencedImplicitAnyParam(params) { }
+}
